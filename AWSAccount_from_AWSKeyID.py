b11ab5db802eff4b47f8701e3468f2a230d3316f
Tal Be'ery <tal.a.beery@gmail.com>
2023-10-25 09:53:43 +0300

    initial commit

---
+++ AWSAccount_from_AWSKeyID.py
@@ -0,0 +1,17 @@
+import base64
+import binascii
+
+def AWSAccount_from_AWSKeyID(AWSKeyID):
+    
+    trimmed_AWSKeyID = AWSKeyID[4:] #remove KeyID prefix
+    x = base64.b32decode(trimmed_AWSKeyID) #base32 decode
+    y = x[0:6]
+    
+    z = int.from_bytes(y, byteorder='big', signed=False)
+    mask = int.from_bytes(binascii.unhexlify(b'7fffffffff10'), byteorder='big', signed=False)
+    
+    e = (z & mask)>>7
+    return (e)
+
+
+print ("account id:" + str(AWSAccount_from_AWSKeyID("ASIAY34FZKBOKMUTVV7A")))
